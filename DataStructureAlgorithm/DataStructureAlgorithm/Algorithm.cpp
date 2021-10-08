38dfa572f2d421dc7ca22ee996534620adc76220
ParkHD <76206050+ParkHD@users.noreply.github.com>
2021-10-08 21:22:05 +0900

    Update Algorithm.cpp

--- DataStructureAlgorithm/DataStructureAlgorithm/Algorithm.cpp
+++ DataStructureAlgorithm/DataStructureAlgorithm/Algorithm.cpp
@@ -36,41 +36,4 @@ int main()
 	bst.Print();
 	this_thread::sleep_for(1s);
 	
-
-	//bst.Insert(30);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(10);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(20);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(25);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(40);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(50);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(15);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(60);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(5);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(22);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(23);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
-	//bst.Insert(21);
-	//bst.Print();
-	//this_thread::sleep_for(1s);
 }
